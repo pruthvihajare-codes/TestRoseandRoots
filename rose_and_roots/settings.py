d888d7e0b22ca7a83012d3b3624c44444c4b4316
Pruthvi <pruthvihajare29@gmail.com>
2026-02-22 00:08:08 +0530

    asd

--- rose_and_roots/settings.py
+++ rose_and_roots/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--h)%86%!jheg&fhm(lp6zk14=&1hg4%5@&ovq-a(2xh27^!fmz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = [ 'localhost', '127.0.0.1' ]
 ALLOWED_HOSTS = ['littlecraftone.pythonanywhere.com']

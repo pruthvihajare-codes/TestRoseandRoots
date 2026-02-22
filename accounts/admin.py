4261eed582c0a91e1d52d731881aa103a63105d7
Pruthvi <pruthvihajare29@gmail.com>
2026-02-22 12:36:47 +0530

    asasd

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import CustomUser, Roles, PasswordStorage, ErrorLog
+from .models import *
 
 admin.site.register(CustomUser)
 admin.site.register(Roles)
 admin.site.register(PasswordStorage)
 admin.site.register(ErrorLog)
+
+print("Accounts admin registered!")
--- masters/admin.py
+++ masters/admin.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
-from .models import (
-    Occasion,
-    Bouquet,
-    BouquetOccasion,
-    BouquetImage,
-    Vendor,
-    DeliveryPincode
-)
+from .models import *
 
+# Register all models simply
 admin.site.register(Occasion)
 admin.site.register(Bouquet)
 admin.site.register(BouquetOccasion)
 admin.site.register(BouquetImage)
 admin.site.register(Vendor)
 admin.site.register(DeliveryPincode)
+
+# Print confirmation (you'll see this in the console when Django loads)
+print("Masters admin registered!")
